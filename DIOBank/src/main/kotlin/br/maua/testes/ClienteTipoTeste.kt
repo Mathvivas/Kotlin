f1baf746474cb5d72b97928da0b37ea00968284a
Matheus Lopes Vivas <mathvivas@hotmail.com>
2021-02-10 17:10:33 -0300

    Update testes

--- DIOBank/src/main/kotlin/br/maua/testes/ClienteTipoTeste.kt
+++ DIOBank/src/main/kotlin/br/maua/testes/ClienteTipoTeste.kt
@@ -6,4 +6,7 @@ fun main() {
     ClienteTipo.values().forEach { elemento ->
         println("${elemento.name} - ${elemento.descricao}")
     }
+
+    val pf = ClienteTipo.PF
+    println("${pf.name} - ${pf.descricao}")
 }
