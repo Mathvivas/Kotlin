78c3cd9fcd02fd4bd1067582ca999a747c413577
Matheus Lopes Vivas <mathvivas@hotmail.com>
2021-02-10 16:16:20 -0300

    Update Pessoa

--- DIOBank/src/main/kotlin/br/maua/Pessoa.kt
+++ DIOBank/src/main/kotlin/br/maua/Pessoa.kt
@@ -3,6 +3,8 @@ package br.maua
 class Pessoa {
     var nome: String = "Matheus"
     var cpf: String = "123.123.123-11"
+    //private var cpf: String = "123.123.123-11"    // Tanto o set, quanto o get são privados
+    private set     // Somete o set é privado, o get é público
 }
 
 fun main() {
@@ -10,4 +12,5 @@ fun main() {
 
     println(math.nome)
     println(math.cpf)
+
 }
