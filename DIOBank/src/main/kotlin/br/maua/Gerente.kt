e66ad60b2d585b5b75b9b2a145ef8adf8b808494
Matheus Lopes Vivas <mathvivas@hotmail.com>
2021-02-10 18:01:18 -0300

    Gerente

---
+++ DIOBank/src/main/kotlin/br/maua/Gerente.kt
@@ -0,0 +1,6 @@
+package br.maua
+
+class Gerente(nome: String, cpf: String, salario: Double): Funcionario(nome, cpf, salario) {
+
+    override fun calculoAuxilio(): Double = salario * 0.4
+}
---
+++ DIOBank/src/main/kotlin/br/maua/testes/GerenteTeste.kt
@@ -0,0 +1,8 @@
+package br.maua.testes
+
+import br.maua.Gerente
+
+fun main() {
+    val gerente = Gerente("Mariana", "768954769806", 5000.0)
+    imprimeRelatorio(gerente)   // função pública
+}
