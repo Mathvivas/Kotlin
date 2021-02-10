4cab579d28fda45852edb85df1caab47ac86d70b
Matheus Lopes Vivas <mathvivas@hotmail.com>
2021-02-10 11:39:03 -0300

    Básico de Strings

---
+++ Basic/src/main/kotlin/strings/concatenacao.kt
@@ -0,0 +1,11 @@
+package strings
+
+fun main() {
+    val name = "Math"
+    val s = "Olá"
+
+    println(s + name)
+    
+    println("${s}, ${name}!")
+    println("Bem vindo, $name!")
+}
---
+++ Basic/src/main/kotlin/strings/indexacao.kt
@@ -0,0 +1,11 @@
+package strings
+
+fun main() {
+    val s = "Olá, mundo!"
+
+    println(s[0])
+    println(s.first())
+
+    println(s[s.length-1])
+    println(s.last())
+}
