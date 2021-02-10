404fd119e111e003128c924e0a3749de81a65ae4
Matheus Lopes Vivas <mathvivas@hotmail.com>
2021-02-10 14:10:37 -0300

    Update Sistema

--- Calculadora/src/main/kotlin/sistema.kt
+++ Calculadora/src/main/kotlin/sistema.kt
@@ -1,3 +1,25 @@
 class Sistema {
 
+    fun run() {
+
+        var op: String?
+        var primeiroValor: Float
+        var segundoValor: Float
+
+        do {
+            op = Menu().menu()
+
+            when (op) {
+                "1" -> { soma() }
+                "2" -> { subtrac() }
+                "3" -> { multiplic() }
+                "4" -> { divid() }
+                "5" -> { fatorial() }
+                "6" -> { potenciac() }
+                "0" -> { println("Obrigado por utilizar a calculadora!") }
+                else -> { println("Valor não existe no Menu, tente novamente!") }
+            }
+
+        } while (op != "0")
+    }
 }
