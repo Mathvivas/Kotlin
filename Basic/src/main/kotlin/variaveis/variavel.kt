e9312dd37d39dd0dacd5a4e028f78c5e6ed85860
Matheus Lopes Vivas <mathvivas@hotmail.com>
2021-02-10 10:50:36 -0300

    Update variavel.kt

--- Basic/src/main/kotlin/variaveis/variavel.kt
+++ Basic/src/main/kotlin/variaveis/variavel.kt
@@ -1,2 +1,22 @@
 package variaveis
 
+const val MAX_AGE = 70
+
+fun main() {
+    val nome:String     // Para declarar uma variável, é necessário definir o tipo
+    var idade = 30
+
+    println(idade)
+    idade = 40
+    println(idade)
+
+    nome = "Math"
+    println(nome)
+
+    // É possível modificar o valor de var, mas não o tipo
+    var ano = "ano"
+    // ano = 2021   -->    Erro
+    println(ano)
+    ano = 2021.toString()
+    println(ano)
+}
