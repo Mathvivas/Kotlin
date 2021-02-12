44ddf639e05cef756efa71e513ee851a8a622395
Matheus Lopes Vivas <mathvivas@hotmail.com>
2021-02-12 17:01:30 -0300

    Melhoria do Arquivo Operações

--- Collections/src/main/kotlin/array/TesteOperacoes.kt
+++ Collections/src/main/kotlin/array/TesteOperacoes.kt
@@ -7,8 +7,8 @@ fun main() {
 
     println()
 
-    println("Maior Salário: ${salarios.max()}")
-    println("Menor Salário: ${salarios.min()}")
+    println("Maior Salário: ${salarios.maxOrNull()}")
+    println("Menor Salário: ${salarios.minOrNull()}")
     println("Média Salarial: ${salarios.average()}")
 
     println()
