42a15f22fb950d37385df80606d7cc1b94a517f8
Matheus Lopes Vivas <mathvivas@hotmail.com>
2021-02-12 16:58:32 -0300

    Exemplo das Operações

---
+++ Collections/src/main/kotlin/array/TesteOperacoes.kt
@@ -0,0 +1,29 @@
+package array
+
+fun main() {
+    val salarios = doubleArrayOf(1400.0, 2750.0, 4600.0)
+
+    salarios.forEach { salario -> println(salario) }
+
+    println()
+
+    println("Maior Salário: ${salarios.max()}")
+    println("Menor Salário: ${salarios.min()}")
+    println("Média Salarial: ${salarios.average()}")
+
+    println()
+
+    val salarioMaiorQue = salarios.filter { it > 2500.0 }
+    salarioMaiorQue.forEach { println(it) }
+
+    println()
+
+    println("count = " + salarios.count { it in 2000.0..5000.0 })
+
+    println()
+
+    println("find = " + salarios.find { it == 2750.0 })
+    println("find = " + salarios.find { it == 3200.0 })
+    println("any = " + salarios.any { it == 1400.0 })
+    println("any = " + salarios.any { it == 1000.0 })
+}
