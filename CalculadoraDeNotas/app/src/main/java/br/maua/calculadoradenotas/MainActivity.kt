30209baadca36f294c754a7153372940578b2a7d
Matheus Lopes Vivas <mathvivas@hotmail.com>
2021-02-12 15:04:54 -0300

    Melhorias

--- CalculadoraDeNotas/app/src/main/java/br/maua/calculadoradenotas/MainActivity.kt
+++ CalculadoraDeNotas/app/src/main/java/br/maua/calculadoradenotas/MainActivity.kt
@@ -1,5 +1,6 @@
 package br.maua.calculadoradenotas
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -21,9 +22,11 @@ class MainActivity : AppCompatActivity() {
 
             if ( media >= 6 && faltas <= 10 ) {
                 resultado.setText("Aluno foi Aprovado!" + "\n" + "Nota Final: " + media + "\n" + "Faltas: " + faltas)
+                resultado.setTextColor(Color.GREEN)
 
             } else {
                 resultado.setText("Aluno foi Reprovado!" + "\n" + "Nota Final: " + media + "\n" + "Faltas: " + faltas)
+                resultado.setTextColor(Color.RED)
             }
         }
     }
