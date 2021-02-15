883b26bc2cac1b937256c39c1ec7a61fad98bf4f
Matheus Lopes Vivas <mathvivas@hotmail.com>
2021-02-15 20:36:13 -0300

    Update Main

--- CalculadoraIMC/app/src/main/java/br/maua/calculadoraimc/MainActivity.kt
+++ CalculadoraIMC/app/src/main/java/br/maua/calculadoraimc/MainActivity.kt
@@ -3,13 +3,41 @@ package br.maua.calculadoraimc
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import androidx.core.widget.doAfterTextChanged
+import androidx.core.widget.doOnTextChanged
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setListeners()
         Log.w("lifecycle", "entrei no CREATE - estou criando a tela")
         // printado no Logcat
+    }
+
+    private fun setListeners() {
+        alturaEDT?.doAfterTextChanged { text ->
+            Toast.makeText(this, text.toString(), Toast.LENGTH_SHORT).show()
+        }
+
+        pesoEDT?.doOnTextChanged { text, _, _, _ ->
+            titleTXT?.text = text
+        }
+
+        calcularBTN?.setOnClickListener {
+            calcularIMC(pesoEDT.text.toString(), alturaEDT.text.toString())
+        }
+    }
+
+    private fun calcularIMC(peso: String, altura: String) {
+        val peso = peso.toFloatOrNull()
+        val altura = altura.toFloatOrNull()
+        if ( peso != null && altura != null ) {
+            val imc = peso / (altura * altura)
+            titleTXT.text = "Seu IMC é %.2f".format(imc)
+        }
     }
 
     override fun onStart() {
