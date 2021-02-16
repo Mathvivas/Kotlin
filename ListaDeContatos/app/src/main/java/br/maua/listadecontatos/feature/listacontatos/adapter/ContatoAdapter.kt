06ec480966398ebc6c4643b10c5c523c5dffe2dd
Matheus Lopes Vivas <mathvivas@hotmail.com>
2021-02-16 13:50:26 -0300

    item_contato.xml

--- ListaDeContatos/app/src/main/java/br/maua/listadecontatos/feature/listacontatos/adapter/ContatoAdapter.kt
+++ ListaDeContatos/app/src/main/java/br/maua/listadecontatos/feature/listacontatos/adapter/ContatoAdapter.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.view.menu.ActionMenuItemView
 import androidx.recyclerview.widget.RecyclerView
 import br.maua.listadecontatos.R
 import br.maua.listadecontatos.feature.listacontatos.model.ContatosVO
